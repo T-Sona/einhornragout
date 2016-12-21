fece7f9533e69b5926caec4c6cfb25ec4064ecb1
Martin Schabel 1522880 <schabel@gmx.de>
2016-12-21 19:30:13 +0100

    Changed: change-picture components - mschabel

--- DEV_Angular2/src/app/change-picture/change-picture.component.ts
+++ DEV_Angular2/src/app/change-picture/change-picture.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiCommunicatorService } from '../api-communicator.service';
 
 @Component({
   selector: 'app-change-picture',
   templateUrl: './change-picture.component.html',
-  styleUrls: ['./change-picture.component.css']
+  styleUrls: ['./change-picture.component.css'],
+  providers: [ApiCommunicatorService]
 })
 export class ChangePictureComponent implements OnInit {
 
-  constructor() { }
+  public avatarPictures = [];
 
-  ngOnInit() {
+  constructor(private apiCommunicatorService: ApiCommunicatorService) {
   }
 
+  ngOnInit() {
+    this.apiCommunicatorService.getAvatars();
+    this.getAvatars();
+  }
+
+  private getAvatars(){
+    this.apiCommunicatorService.getAvatars()
+      .subscribe((avatar: Array<Object>) => this.avatarPictures = avatar);
+  }
 }
