befa2071f243869bea7f4a5faf5f8e6a7fa79703
Johannes Schmid 1527565 <JohannesS92@web.de>
2016-12-22 15:01:54 +0100

    Changed: change-password - joschmid

--- DEV_Angular2/src/app/change-password/change-password.component.ts
+++ DEV_Angular2/src/app/change-password/change-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+
 
 }
