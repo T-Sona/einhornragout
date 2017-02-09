d32548c511a0c24ca872b7aa65f510c26700fb9f
Johannes Schmid 1527565 <JohannesS92@web.de>
2017-02-09 18:00:28 +0100

    Changed: home - Login mit Enter-Taste - joschmid

--- DEV_Angular2/src/app/home/home-body/home-body.component.ts
+++ DEV_Angular2/src/app/home/home-body/home-body.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { LoginService } from '../../login.service';
-import { ErrorService } from '../../error.service';
-import { Router } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {LoginService} from '../../login.service';
+import {ErrorService} from '../../error.service';
+import {Router} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-body',
@@ -13,23 +13,30 @@ import { Title } from '@angular/platform-browser';
 export class HomeBodyComponent implements OnInit {
 
 
-  constructor(private loginService: LoginService, private router: Router, private errorService: ErrorService,private titleService: Title) {
+  constructor(private loginService: LoginService, private router: Router, private errorService: ErrorService, private titleService: Title) {
   }
 
   ngOnInit() {
-    this.titleService.setTitle( "CHECK! - Login" );
-    console.log("LoggedIn:"+this.loginService.isLoggedIn());
-    if(this.loginService.isLoggedIn()){
+    this.titleService.setTitle("CHECK! - Login");
+    console.log("LoggedIn:" + this.loginService.isLoggedIn());
+    if (this.loginService.isLoggedIn()) {
       this.router.navigate(["../main"]);
     }
   }
 
-  private login(username,password,savedLogin){
-    console.log("username: "+username+" | password: "+password+" | savedLogin:"+!!savedLogin);
-    this.loginService.login(username,password,!!savedLogin).subscribe(res => this.router.navigate(["../main"]), (err) => {
-          this.errorService.throwError("Ihre Eingaben sind nicht korrekt, bitte versuchen sie es erneut!");});
+  private login(username, password, savedLogin) {
+    console.log("username: " + username + " | password: " + password + " | savedLogin:" + !!savedLogin);
+    this.loginService.login(username, password, !!savedLogin).subscribe(res => this.router.navigate(["../main"]), (err) => {
+      this.errorService.throwError("Ihre Eingaben sind nicht korrekt, bitte versuchen sie es erneut!");
+    });
   }
 
   showDialog = false;
 
+
 }
+
+
+
+
+
